1c1f49a6cca6994c5e5288b2a045d6f2cb4efc4f
byan1197 <Bondyan97@gmail.com>
2019-03-04 14:22:12 -0500

    added IG stuff. not in working state.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const http = require('http')
 const app = express();
 const cors = require('cors');
 const playgroundRoutes = require('./playground');
+const igPlaygroundRoutes = require('./igplayground');
 
 const config = require('./config.json');
 const OAuth = require('oauth');
@@ -31,46 +32,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/twitter/authorize', playgroundRoutes);
+app.use('/ig/', igPlaygroundRoutes);
 
 app.get('/', (req, res) => {
     res.send('<h1>WOW!!!!!!!</h1>')
 });
 
-var twitter = new Twitter(config.twitter);
-var success = function(data){
-    console.log("SUCCESS", data)
-}
-var oauth_one = function (data) {
-    process.env.TOKEN = data.token;
-    process.env.TOKEN_SECRET = data.token_secret;
-    console.log('process.env.TOKEN', process.env.TOKEN)
-    console.log('process.env.TOKEN_SECRET', process.env.TOKEN_SECRET)
-};
-
-
-twitter.getOAuthRequestToken(oauth_one)
-// const oauth = {
-//     token : 'DoT7MgAAAAAA9chhAAABaOTmQt4',
-//     token_secret : 'doi7WiFGauG6GDaUimi4YFpU8bxapoxx'
+// var twitter = new Twitter(coer = new Twitter(config.twitter);
+// var success = function(data){
+//     console.log("SUCCESS", data)
 // }
+// var oauth_one = function (data) {
+//     process.env.TOKEN = data.token;
+//     process.env.TOKEN_SECRET = data.token_secret;
+//     console.log('process.env.TOKEN', process.env.TOKEN)
+//     console.log('process.env.TOKEN_SECRET', process.env.TOKEN_SECRET)
+// };
 
 
-//Get this data from your twitter apps dashboard
+// twitter.getOAuthRequestToken(oauth_one)
+    // nfig.twitter);
+// var success = function(data){
+//     console.log("SUCCESS", data)
+// }
+// var oauth_one = function (data) {
+//     process.env.TOKEN = data.token;
+//     process.env.TOKEN_SECRET = data.token_secret;
+//     console.log('process.env.TOKEN', process.env.TOKEN)
+//     console.log('process.env.TOKEN_SECRET', process.env.TOKEN_SECRET)
+// };
 
 
-// make a directory in the root folder of your project called data
-// copy the node_modules/twitter-node-client/twitter_config file over into data/twitter_config`
-// Open `data/twitter_config` and supply your applications `consumerKey`, 'consumerSecret', 'accessToken', 'accessTokenSecret', 'callBackUrl' to the appropriate fields in your data/twitter_config file
-
-
-
-//twitter.getUserTimeline({ screen_name: 'DeskNibbles', count: '10'}, error, success);
-	
-//twitter.getTweet({ id: '1111111111'}, error, success);
-	
-	//
-	// Get 10 tweets containing the hashtag haiku
-    //
+// twitter.getOAuthRequestToken(oauth_one)
     
 http.createServer(app).listen(port, function () {
     console.log('Our project is running! ', (new Date()).toString());
