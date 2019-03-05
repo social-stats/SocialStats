1bbf9f3a6759b3e0615396dc54fa89f24a9f94d3
byan1197 <Bondyan97@gmail.com>
2019-03-05 17:57:43 -0500

    quick api changes

--- igplayground.js
+++ igplayground.js
@@ -12,10 +12,10 @@ var fb = new FB.Facebook({
 router.get('/', (req, res, next) => {
   res.send('<a href="' + FB.getLoginUrl({
     client_id: process.env.FACEBOOK_APP_ID,
-    scope: 'email,user_likes, r_posts, manage_pages',
+    scope: ['email', 'user_likes', 'manage_pages', 'instagram_basic'],
     redirect_uri: process.env.FACEBOOK_REDIRECT,
     // redirect_uri: 'test.com',
-  })+ '">Sign in with facebook/instagram</a>')
+  }) + '">Sign in with facebook/instagram</a>')
 });
 
 router.get('/auth', (req, res, next) => {
