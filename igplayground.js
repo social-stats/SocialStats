67004e88c2bd257cffe64f80269cb7827636750d
byan1197 <Bondyan97@gmail.com>
2019-03-05 16:04:09 -0500

    got facebook auth working. next step is to work on changing auth privileges

--- igplayground.js
+++ igplayground.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dotEnv = require('dotenv').config();
 var FB = require('fb');
 var fb = new FB.Facebook({
+  appId: process.env.FACEBOOK_APP_ID,
   autoLogAppEvents: true,
   xfbml: true,
   version: 'v3.2'
@@ -10,13 +11,14 @@ var fb = new FB.Facebook({
 
 router.get('/', (req, res, next) => {
   res.send('<a href="' + FB.getLoginUrl({
-    client_id: process.env.FACEBOOK_CLIENT_ID,
-    scope: 'email,user_likes',
+    client_id: process.env.FACEBOOK_APP_ID,
+    scope: 'email,user_likes, r_posts, manage_pages',
     redirect_uri: process.env.FACEBOOK_REDIRECT,
+    // redirect_uri: 'test.com',
   })+ '">Sign in with facebook/instagram</a>')
 });
 
-router.get('/authorize', (req, res, next) => {
+router.get('/auth', (req, res, next) => {
   res.send(req.query);
 });
 
