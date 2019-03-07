23e40d3c5b683c5ca4a20c72fcd3f5e72215f7a9
Praiyon <praiyon@gmail.com>
2019-03-07 00:14:24 -0500

    fixed twitter callback errors

--- twitter_playground.js
+++ twitter_playground.js
@@ -21,8 +21,9 @@ router.get('/callback', (req, res, next) => {
     
     axios.post(`https://api.twitter.com/oauth/access_token?oauth_consumer_key=${process.env.TWITTER_CONSUMER_KEY}&oauth_token=${oauth_token}&oauth_verifier=${oauth_verifier}`, {})
       .then(function (response) {
-          
-        res.send(response.data);
+        res.send({
+            oauth_token : response.data
+        });
       })
       .catch(function (error) {
         console.log(error);
