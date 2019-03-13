1bfefc1048cb3d0c117d70c6127adadd59da34fe
byan1197 <Bondyan97@gmail.com>
2019-03-12 21:24:37 -0400

    more testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/www/SocialStats
 npm run build
--- scripts/packageInstall.sh
+++ scripts/packageInstall.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/www/SocialStats
 sudo npm i -g pm2 
