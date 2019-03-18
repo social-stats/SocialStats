fbab71699510ae498b16a5fdf42ed1ce93cf1ae5
byan1197 <Bondyan97@gmail.com>
2019-03-18 18:35:18 -0400

    twitter weekly snapshot bulk is done. need some tweaking and need to save to mongo now.

--- twitter_fetcher.js
+++ twitter_fetcher.js
@@ -4,141 +4,142 @@ const dotEnv = require('dotenv').config();
 var Twitter = require('twitter-node-client').Twitter;
 const axios = require('axios');
 
-const twitter_config =  {
+const twitter_config = {
 
-    consumerKey : process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret  : process.env.TWITTER_CONSUMER_SECRET,
-    accessToken : '1093514095976886273-s7HQWwShY4b7MvPjCrVnvmdwPdSxAK',
-    accessTokenSecret : 'AnUyYZNz6thaMJjTIVsKwJFi8ivl30IxoTXIc9vGxE8QI',
-    callBackUrl : process.env.TWITTER_CALLBACK_URL
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: '1093514095976886273-s7HQWwShY4b7MvPjCrVnvmdwPdSxAK',
+  accessTokenSecret: 'AnUyYZNz6thaMJjTIVsKwJFi8ivl30IxoTXIc9vGxE8QI',
+  callBackUrl: process.env.TWITTER_CALLBACK_URL
 }
 
 var twitter = new Twitter(twitter_config);
 
 const client_service = (token = null) => {
   const defaultOptions = {
-      headers: {
-          Authorization: token ? `${token}` : '',
-      },
+    headers: {
+      Authorization: token ? `${token}` : '',
+    },
   };
 
   return {
-      get: (url, options = {}) => axios.get(url, { ...defaultOptions, ...options }),
-      post: (url, data, options = {}) => axios.post(url, data, { ...defaultOptions, ...options }),
-      put: (url, data, options = {}) => axios.put(url, data, { ...defaultOptions, ...options }),
-      delete: (url, options = {}) => axios.delete(url, { ...defaultOptions, ...options }),
+    get: (url, options = {}) => axios.get(url, { ...defaultOptions, ...options }),
+    post: (url, data, options = {}) => axios.post(url, data, { ...defaultOptions, ...options }),
+    put: (url, data, options = {}) => axios.put(url, data, { ...defaultOptions, ...options }),
+    delete: (url, options = {}) => axios.delete(url, { ...defaultOptions, ...options }),
   };
 };
 
-const TwitterFetcher =  {
+const TwitterFetcher = {
 
-    getRequestToken: function () {
-        return new Promise((res) => twitter.getOAuthRequestToken( data => {
-            res(data)
-        }));
-    },
-  
-    getHomeTimeline: (client) =>{
-      const twitter_client = new Twitter(client)
-      
-      // console.log('HERE', client[0].consumerKey)
-      // const request = client_service(`OAuth oauth_consumer_key="9wkjjUziuqqsHMPyEzB2PKKQd",oauth_token="1093514095976886273-s7HQWwShY4b7MvPjCrVnvmdwPdSxAK",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1552270993",oauth_nonce="tuCvTVNXJ5S",oauth_version="1.0",oauth_signature="zyY1jbcQeJ%2Bk6cYAcx10%2BWLDhzE%3D"`);
-      // request.get('https://api.twitter.com/1.1/statuses/home_timeline.json').then(x => console.log(x))
-      return new Promise( (res)=>
+  getRequestToken: function () {
+    return new Promise((res) => twitter.getOAuthRequestToken(data => {
+      res(data)
+    }));
+  },
+
+  getHomeTimeline: (client) => {
+    const twitter_client = new Twitter(client)
+
+    // console.log('HERE', client[0].consumerKey)
+    // const request = client_service(`OAuth oauth_consumer_key="9wkjjUziuqqsHMPyEzB2PKKQd",oauth_token="1093514095976886273-s7HQWwShY4b7MvPjCrVnvmdwPdSxAK",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1552270993",oauth_nonce="tuCvTVNXJ5S",oauth_version="1.0",oauth_signature="zyY1jbcQeJ%2Bk6cYAcx10%2BWLDhzE%3D"`);
+    // request.get('https://api.twitter.com/1.1/statuses/home_timeline.json').then(x => console.log(x))
+    return new Promise((res) =>
       twitter_client.getHomeTimeline({
         count: 200
       }, (e) => {
         console.log('get mentions TL err', e)
       }, (result) => {
         JSON.parse(result).map(twitter_object => {
-          res( {
+          res({
             tweet: twitter_object.text,
             favourites: twitter_object.favorite_count,
             retweets: twitter_object.retweet_count,
             id: twitter_object.id,
             date: twitter_object.created_at.toString()
           })
- 
-        })
-      }))},
-      getUserTimeline: (name) => {
-        return new Promise( (res)=>{
-        const tweets = []
 
-        twitter.getUserTimeline({
-          screen_name: name,
-          count: 200
-        }, (e) => {
-          console.log('get user TL err', name);
-        }, (result) => {
-          const jsonResult = JSON.parse(result);
-          
-          jsonResult.forEach(tweet_object => {
-            
-            tweets.push({
-              tweet: tweet_object.text,
-              favourites: tweet_object.favorite_count,
-              retweets: tweet_object.retweet_count,
-              tweetId: tweet_object.id_str,
-              date: tweet_object.created_at,
-              name: name
-            })
-          })
-          
-            res({
-              followers: jsonResult[0].user.followers_count,
-              tweets: tweets
-            })
-          
-          }
-          
-            
-          
-          //const numFollowers = JSON.parse(result)[0].user.followers_count;
-          //console.log('Followers: ', numFollowers)
-        
-         ) });
-    },
-    getMentionsTimeLine: (client) => {
-      client.getMentionsTimeline({
-        count: 10
-      }, (e) => {
-        console.log('get mentions TL err', e)
-      }, (result) => {
-        console.log('Mentions: ', result)
-      });
-    },
-    getRetweetsOfMe: () => {
-      twitter.getReTweetsOfMe({
-        include_user_entities: true
-      }, (e) => {
-        console.log('retweets of me err: ', e)
-      }, (result) => {
-        console.log('Tweets that have been retweeted: ', result)
-      })
-    },
-    getTrendsNearMe: (woeid) => {
-      twitter.getCustomApiCall('/trends/place.json', {
-        id: woeid
-      }, (e) => {
-        console.log('get trends near me err: ', e)
-      }, (result) => {
-        console.log('get trends near me: ', result)
-      })
-    },
-    getSearchResults: (searchQuery) => {
-      return new Promise(res =>{
-        twitter.getSearch({
-          q: searchQuery,
-          count: 200
-        }, (e) => {
-          console.log('search err: ', e)
-        }, (result) => {
-          res(JSON.parse(result))
         })
+      }))
+  },
+  getUserTimeline: (name, params) => {
+    return new Promise((res) => {
+      const tweets = []
+
+      twitter.getUserTimeline({
+        ...params,
+        screen_name: name,
+        count: 200
+      }, (e) => {
+        console.log('get user TL err', name);
+      }, (result) => {
+        const jsonResult = JSON.parse(result);
+
+        jsonResult.forEach(tweet_object => {
+
+          tweets.push({
+            tw_user: tweet_object.user,
+            tweet: tweet_object.text,
+            favourites: tweet_object.favorite_count,
+            retweets: tweet_object.retweet_count,
+            tweetId: tweet_object.id_str,
+            date: tweet_object.created_at,
+            name: name
+          })
+        })
+
+        res({
+          followers: jsonResult[0].user.followers_count,
+          tweets: tweets
+        })
+
+      }
+        //const numFollowers = JSON.parse(result)[0].user.followers_count;
+        //console.log('Followers: ', numFollowers)
+
+      )
+    });
+  },
+  getMentionsTimeLine: (client) => {
+    client.getMentionsTimeline({
+      count: 10
+    }, (e) => {
+      console.log('get mentions TL err', e)
+    }, (result) => {
+      console.log('Mentions: ', result)
+    });
+  },
+  getRetweetsOfMe: () => {
+    twitter.getReTweetsOfMe({
+      include_user_entities: true
+    }, (e) => {
+      console.log('retweets of me err: ', e)
+    }, (result) => {
+      console.log('Tweets that have been retweeted: ', result)
+    })
+  },
+  getTrendsNearMe: (woeid) => {
+    twitter.getCustomApiCall('/trends/place.json', {
+      id: woeid
+    }, (e) => {
+      console.log('get trends near me err: ', e)
+    }, (result) => {
+      console.log('get trends near me: ', result)
+    })
+  },
+  getSearchResults: (searchQuery) => {
+    return new Promise(res => {
+      twitter.getSearch({
+        q: searchQuery,
+        count: 200
+      }, (e) => {
+        console.log('search err: ', e)
+      }, (result) => {
+        res(JSON.parse(result))
       })
-      
-    },
+    })
+
+  },
 }
 
 module.exports = TwitterFetcher;
--- twitter_helper.js
+++ twitter_helper.js
@@ -1,45 +1,51 @@
 const dotEnv = require('dotenv').config();
-const User = require('./models/user_object');
-const mongoose = require('mongoose');
-const Tweet = require('./models/tweet');
 const TwitterFetcher = require('./twitter_fetcher');
 const _ = require('lodash')
+const moment = require('moment')
+
+const mongoose = require('mongoose');
+const User = require('./models/user_object');
+const Tweet = require('./models/tweet');
+const TiwtterSnapshot = require('./models/twitter_snapshot')
+
+
 const TwitterHelper = {
-    getListOfClients: () =>{
+    getListOfClients: () => {
         const twitter_objects = [];
-        return(
-        User
-            .find()
-            .select('twitter')
-            .then(results =>{
-                results.forEach(client =>                  
-                    twitter_objects.push( {
-                        userId: client._id,
-                        name: client.twitter.name,
-                        id: client.twitter.id,
-                        consumerKey : process.env.TWITTER_CONSUMER_KEY,
-                        consumerSecret  : process.env.TWITTER_CONSUMER_SECRET,
-                        accessToken : client.twitter.access_token,
-                        accessTokenSecret : client.twitter.token_secret,
-                        callBackUrl : process.env.TWITTER_CALLBACK_URL
-                    }))
-                return twitter_objects;
-            })
+        return (
+            User
+                .find()
+                .select('twitter')
+                .then(results => {
+                    results.forEach(client =>
+                        twitter_objects.push({
+                            userId: client._id,
+                            name: client.twitter.name,
+                            id: client.twitter.id,
+                            consumerKey: process.env.TWITTER_CONSUMER_KEY,
+                            consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+                            accessToken: client.twitter.access_token,
+                            accessTokenSecret: client.twitter.token_secret,
+                            callBackUrl: process.env.TWITTER_CALLBACK_URL
+                        }))
+                    return twitter_objects;
+                })
         )
-    }}
+    }
+}
 
 getTweets = (id) => new Promise((res) => {
-    
-        
-  })
+
+
+})
 const updateReplies = (unfinishedTweets, name) => {
     let newTweets = unfinishedTweets.newTweets
     let updatedTweets = unfinishedTweets.updatedTweets
 
     let searchResultMap = new Map();
-    return(
+    return (
 
-    
+
         TwitterFetcher.getSearchResults(`@${name}`)
             .then(res => {
                 res.statuses.forEach(tweet => {
@@ -52,67 +58,67 @@ const updateReplies = (unfinishedTweets, name) => {
                     }
                 })
 
-                newTweets.forEach(tweet =>{
+                newTweets.forEach(tweet => {
                     let numberOfReplies = searchResultMap.get(tweet.tweetId)
-                    if(numberOfReplies) tweet['replies'] = numberOfReplies
-                    else if(!tweet.hasOwnProperty('replies')) tweet['replies'] = numberOfReplies
+                    if (numberOfReplies) tweet['replies'] = numberOfReplies
+                    else if (!tweet.hasOwnProperty('replies')) tweet['replies'] = numberOfReplies
                 })
 
                 updatedTweets.forEach(tweet => {
                     let numberOfReplies = searchResultMap.get(tweet.tweetId)
-                    if(numberOfReplies) tweet['replies'] = numberOfReplies
-                    else if(!tweet.hasOwnProperty('replies')) tweet['replies'] = numberOfReplies
+                    if (numberOfReplies) tweet['replies'] = numberOfReplies
+                    else if (!tweet.hasOwnProperty('replies')) tweet['replies'] = numberOfReplies
                 })
 
-                return({
+                return ({
                     newTweets: newTweets,
                     db_tweets: updatedTweets
                 })
             })
-        )
+    )
 }
 const saveTweets = (allTweets) => {
     const db_tweets = allTweets.db_tweets
     const newTweets = allTweets.newTweets
-    
+
     let mongooseTweets = newTweets.map(tweet => new Tweet({
-                    _id: new mongoose.Types.ObjectId(),
-                    user: tweet.user,
-                    favourites: tweet.favourites,
-                    retweets: tweet.retweets,
-                    tweetId: tweet.tweetId,
-                    date: tweet.date
-                }))
+        _id: new mongoose.Types.ObjectId(),
+        user: tweet.user,
+        favourites: tweet.favourites,
+        retweets: tweet.retweets,
+        tweetId: tweet.tweetId,
+        date: tweet.date
+    }))
     console.log(mongooseTweets)
-    
+
 }
 const compareTweets = (current, id) => {
- 
+
     let newTweets = []
     let updatedTweets = []
     name = current.name
     console.log(current.tweets)
-    return(
+    return (
         Tweet
             .find()
             .exec()
-            .then(results =>{
+            .then(results => {
 
                 const currentTweetsMap = new Map(current.tweets.map(tweet => [tweet.tweetId, tweet]))
                 const dbTweetMap = new Map(results.map(db_tweet => [db_tweet.tweetId, db_tweet]));
                 current.tweets.forEach(tweet => {
-                    if (!dbTweetMap.get(tweet.tweetId)){
+                    if (!dbTweetMap.get(tweet.tweetId)) {
                         tweet['user'] = id
                         newTweets.push(tweet)
                     }
-                        
+
                 })
 
                 console.log(newTweets, 'THAT NEWNEW')
 
                 results.forEach(db_tweet => {
                     tweetFound = currentTweetsMap.get(db_tweet.tweetId)
-                    if(tweetFound){
+                    if (tweetFound) {
                         updatedTweets.push({
                             _id: db_tweet._id,
                             tweet: tweetFound.favourites,
@@ -124,10 +130,10 @@ const compareTweets = (current, id) => {
                     }
                 })
                 console.log(updatedTweets, 'UPDATE')
-     
-                
 
-                return({
+
+
+                return ({
                     newTweets: newTweets,
                     updatedTweets: updatedTweets
                 })
@@ -141,7 +147,7 @@ const compareTweets = (current, id) => {
                 //         console.log("helloTHERE")
                 //     }
                 // })
-                    
+
                 // results.map(db_tweet => db_tweet.)
 
                 // let mongooseTweets = newTweets.map(tweet => new Tweet({
@@ -153,37 +159,117 @@ const compareTweets = (current, id) => {
                 //     date: tweet.date
                 // }))
                 // console.log(mongooseTweets, "HERE")
-            }) 
-    )  
+            })
+    )
 }
 
-    const TwitterScedhuler = {
-        //fetch current tweets G
-        //fetch database tweets G
-            // add new tweets to database G
-            // update fields for tweets in database //lodash pick and merge
-        //update replies to comments 
-        // if tweet is 8days old or older, then delete from database
-        // add tweet count, reply count to week object
+const TwitterScedhuler = {
+    //fetch current tweets G
+    //fetch database tweets G
+    // add new tweets to database G
+    // update fields for tweets in database //lodash pick and merge
+    //update replies to comments 
+    // if tweet is 8days old or older, then delete from database
+    // add tweet count, reply count to week object
 
 
-        initiateTwitterScedhuling: () => {
-            
-            TwitterHelper.getListOfClients()
-                .then(results => 
-                    results.forEach(twitter_client => {
-                        TwitterFetcher.getUserTimeline(twitter_client.name)
-                            .then(tweets => compareTweets(tweets, twitter_client.userId)   
+    initiateTwitterScedhuling: () => {
+
+        TwitterHelper.getListOfClients()
+            .then(results =>
+                results.forEach(twitter_client => {
+                    // tweets in db
+                    TwitterFetcher.getUserTimeline(twitter_client.name)
+                        .then(tweets => compareTweets(tweets, twitter_client.userId)
                             .then(res => updateReplies(res, twitter_client.name))
                             .then(res => saveTweets(res)) // fetch replies
-                        // TwitterFetcher.getUserTimeline(twitter_client.name)
-                        //     .then(result => console.log(result))
-                        // .then(x => console.log(x)) // get 200 most recent tweets
-                        // .then(getTweets())
-                    )}))
-                .then(() => console.log("hello"))
-                .catch(e => console.log(e))  
-        },
+                            // TwitterFetcher.getUserTimeline(twitter_client.name)
+                            //     .then(result => console.log(result))
+                            // .then(x => console.log(x)) // get 200 most recent tweets
+                            // .then(getTweets())
+                        )
+
+                    // snapshot stuff
+                    TwitterFetcher.getUserTimeline(twitter_client.name)
+                        .then(tweet)
+                }))
+            .then(() => console.log("hello"))
+            .catch(e => console.log(e))
+    },
+    runSnapshot: () => {
+
+        var recentSnapshotsPromise = new Promise((resolve, reject) => {
+            TwitterHelper.getListOfClients()
+                .then(results =>
+                    results.forEach(tc => {
+                        TiwtterSnapshot.find({
+                            date: { $gt: moment().startOf('date').subtract(7, 'days') }
+                        })
+                            .sort({ date: -1 })//sorted ASC
+                            .exec((err, snapshots) => {
+                                if (err) reject(err);
+                                snapshots[0].date // earliest date within a week, some days may be skipped
+
+                            })
+                    })
+                )
+        })
+
+        recentSnapshotsPromise.then(snapshots => {
+            // tweet_id of the earliest tweet of the week
+            var since_id = snapshots[0].firstTweetId
+
+            TwitterFetcher.getUserTimeline(tc.name, { since_id: since_id})
+                .then(tl => { //tweet list
+                    tl.tweets.forEach(t => {
+                        var key = moment(t.date).startOf('day').toDate().toString()
+                        var prevObj = obj[key] || {
+                            user: userId,
+                            retweets: 0,
+                            posts: 0,
+                            firstTweetUnix: moment().add(1, 'days').unix(),
+                            firstTweetId: -1
+                        };
+                        obj[key] = {
+                            ...prevObj,
+                            posts: prevObj.posts + 1,
+                            retweets: prevObj.retweets + t.retweets,
+                            firstTweetUnix: prevObj.firstTweetUnix < moment(t.date).unix() ? prevObj.firstTweetUnix : moment(t.date).unix(),
+                            firstTweetId: prevObj.firstTweetUnix < moment(t.date).unix() ? prevObj.firstTweet : t.tweetId
+                        }
+
+                    })
+                })
+        }).catch(err => {
+            console.error('Yikes, an error has occurred: ', err)
+        })
+    },
+    runInitialSnapshot: (userId) => {
+        TwitterFetcher.getUserTimeline('desknibbles')
+            .then(tl => {
+                var obj = {};
+                tl.tweets.forEach(t => {
+                    var key = moment(t.date).startOf('day').toDate().toString()
+                    var prevObj = obj[key] || {
+                        user: userId,
+                        favourites: 0,
+                        retweets: 0,
+                        posts: 0,
+                        firstTweetUnix: moment().add(1, 'days').unix(),
+                        firstTweetId: -1
+                    };
+                    obj[key] = {
+                        ...prevObj,
+                        favourites: prevObj.favourites + t.favourites,
+                        posts: prevObj.posts + 1,
+                        retweets: prevObj.retweets + t.retweets,
+                        firstTweetUnix: prevObj.firstTweetUnix < moment(t.date).unix() ? prevObj.firstTweetUnix : moment(t.date).unix(),
+                        firstTweetId: prevObj.firstTweetUnix < moment(t.date).unix() ? prevObj.firstTweet : t.tweetId
+                    }
+                })
+
+            })
     }
+}
 
 module.exports = TwitterScedhuler;
