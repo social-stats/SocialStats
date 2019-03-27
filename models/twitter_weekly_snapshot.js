7238000ce2f281b250fc34af2370a6d4f798a4e4
Praiyon <praiyon@gmail.com>
2019-03-26 23:35:25 -0400

    weekly snapshot changes

--- models/twitter_weekly_snapshot.js
+++ models/twitter_weekly_snapshot.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
-const twitterWeeklySnapshotSchema = mongoose.Schema({
+const Tweet = require('./tweet');
+const twitterWeeklySnapshotSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     date: { type: mongoose.Schema.Types.Date, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     firstTweetId: { type: mongoose.Schema.Types.Number, required: false },
-    topThreeLiked: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: false}],
+   
     topThreeRetweeted: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: false}],
-    topThreeReplied: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: false}]
+    topThreeFavorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: false}],
+    topThreeReplies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tweet', required: false}]
+    // topThreeReplied: [{Tweet, required: false}]
 });
 
 module.exports = mongoose.model("WeeklySnapshot", twitterWeeklySnapshotSchema);
