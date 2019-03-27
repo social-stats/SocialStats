b2b7da0ec32157fad5222cf4a206e1e773e6799c
Praiyon <praiyon@gmail.com>
2019-03-26 22:35:49 -0400

    new weekly snapshot model

---
+++ models/twitter_weekly_snapshot.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const twitterWeeklySnapshotSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    date: { type: mongoose.Schema.Types.Date, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    firstTweetId: { type: mongoose.Schema.Types.Number, required: false },
+    topThreeLiked: {type: mongoose.Schema.Types.Array, ref: 'Tweet', required: false},
+    topThreeRetweeted: {type: mongoose.Schema.Types.Array, red: 'Tweet', required: false},
+    topThreeReplied: {type: mongoose.Schema.Types.Array, red: 'Tweet', required: false}
+});
+
+module.exports = mongoose.model("WeeklySnapshot", twitterWeeklySnapshotSchema);
